7a8864c557e602567dc609e49ee8600236e8d9c9
George Zygmund <zygmundgw@cardiff.ac.uk>
2022-02-24 07:35:20 +0000

    Extracted NavBar Component

--- angular-organic-store/src/app/app.module.ts
+++ angular-organic-store/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent
   ],
   imports: [
     BrowserModule
