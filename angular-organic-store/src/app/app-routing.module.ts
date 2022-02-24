813e2f6fff88e90d80032f80a29651b00dc9bf02
George Zygmund <zygmundgw@cardiff.ac.uk>
2022-02-24 08:23:06 +0000

    Navbar CleanUp RouterLinks organised.

--- angular-organic-store/src/app/app-routing.module.ts
+++ angular-organic-store/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import {OrderSuccessComponent} from "./order-success/order-success.component";
 import {LoginComponent} from "./login/login.component";
 import {AdminProductsComponent} from "./admin/admin-products/admin-products.component";
 import {AdminOrdersComponent} from "./admin/admin-orders/admin-orders.component";
+import {MyOrdersComponent} from "./my-orders/my-orders.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -15,9 +16,10 @@ const routes: Routes = [
   { path: 'shopping-cart', component: ShoppingCartComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'order-success', component: OrderSuccessComponent },
+  { path: 'my/orders', component: MyOrdersComponent },
   { path: 'login', component: LoginComponent },
   { path: 'admin/products', component: AdminProductsComponent },
-  { path: 'admin/order', component: AdminOrdersComponent },
+  { path: 'admin/orders', component: AdminOrdersComponent },
 ]
 
 @NgModule({
