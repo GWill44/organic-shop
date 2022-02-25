3b1393e18c97b0c4eefe32ebae85a46795641b46
George Zygmund <zygmundgw@cardiff.ac.uk>
2022-02-25 10:55:39 +0000

    Unsubscribing from authStateSubscription
    I have decided to use the 'OnDestroy' interface and implement the '.unsubscribe' method instead of using an Async Pipe.
    I chose this method to improve seperation of concern between the TS logic and the HTML template.

--- angular-organic-store/src/app/bs-navbar/bs-navbar.component.ts
+++ angular-organic-store/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/compat/auth";
 import firebase from "firebase/compat/app";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
-  user: firebase.User | null
+export class BsNavbarComponent implements OnInit, OnDestroy {
+  user: firebase.User | null;
+  private authStateSubscription: Subscription;
 
-  constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(user => this.user = user);
+  constructor(private afAuth: AngularFireAuth) {}
+
+  ngOnInit() {
+    this.authStateSubscription = this.afAuth.authState.subscribe(user => this.user = user);
+  }
+  ngOnDestroy() {
+    this.authStateSubscription.unsubscribe();
   }
 
   logout() {
