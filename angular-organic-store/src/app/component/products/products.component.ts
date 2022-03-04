6438319e6f96842a1251066d03680bd4d52e6a58
George Zygmund <zygmundgw@cardiff.ac.uk>
2022-03-04 19:18:02 +0000

    Category Filter with Muiltiple Asynchronous Ops
    Category filter confirgured.
    
    Error Fixed:
    There was an error where products wouldnt render unless a filter was selected.
    This was due to two seperate asynchronous operations happening when loading the page.
    Error resolved by combining the asynchronous operations via '.pipe()' and '.switchMap()'.

--- angular-organic-store/src/app/component/products/products.component.ts
+++ angular-organic-store/src/app/component/products/products.component.ts
@@ -1,21 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductService} from "../../service/product/product.service";
 import {CategoryService} from "../../service/category/category.service";
+import {ActivatedRoute} from "@angular/router";
+import {Subscription, switchMap} from "rxjs";
+import {Product} from "../../model/product";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
-  products$;
+export class ProductsComponent implements OnInit, OnDestroy {
   categories$;
+  products: Product[] = [];
+  filteredProducts: Product[];
+  productsSub: Subscription;
 
-  constructor(private productService: ProductService, private categoryService: CategoryService) {}
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService,
+    private route: ActivatedRoute) {}
 
   ngOnInit(){
-    this.products$ = this.productService.getAll();
-    this.categories$ = this.categoryService.getAll();
-  }
+    this.productService.getAll().pipe(
+      switchMap((products: any) => {
+        this.products = products;
+        return this.route.queryParamMap;
+      }))
+      .subscribe(params =>{
+        let category = params.get('category');
+        this.filteredProducts = (category) ?
+          this.products.filter(product => product.category === category) :
+          this.products;
+      });
 
+    this.categories$ = this.categoryService.getAll();
+    }
+
+  ngOnDestroy() {
+    this.productsSub.unsubscribe();
+  }
 }
